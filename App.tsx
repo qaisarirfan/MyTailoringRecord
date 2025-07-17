627dc7792543e9ae38c065f345271e892d3f2292
Qaisar Irfan <qaisar.irfan@arbisoft.com>
2025-07-17 10:28:54 +0500

    Remove native-base

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NativeBaseProvider } from 'native-base';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import persistStore from 'redux-persist/lib/persistStore';
@@ -14,9 +13,7 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
-      <NativeBaseProvider>
-        <Screens />
-      </NativeBaseProvider>
+      <Screens />
       {/* </PersistGate> */}
     </Provider>
   );
