dd9e03a3ec7d33e1ee54afef568c1fb0e65fb9b3
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2025-07-26 18:07:55 +0500

    Updated dashboard screen

--- src/screens/secure/dashboard.tsx
+++ src/screens/secure/dashboard.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import { StyleSheet, Button, ScrollView, Text, StatusBar } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { StyleSheet, Button, ScrollView, Text, StatusBar } from "react-native";
+import { Avatar, Card, IconButton, Icon, Divider } from "react-native-paper";
 
 export const Dashboard = () => {
   const email = null;
+  const { navigate } = useNavigation();
   return (
     <ScrollView
       keyboardDismissMode="interactive"
       contentContainerStyle={Styles.scrollView}
     >
-      <Text>{`Welcome ${email}`}</Text>
-      <Button title="Logout" />
+      <Card mode="contained" onPress={() => navigate("AddCustomer")}>
+        <Card.Title
+          title="Add customers"
+          subtitle="Card Subtitle"
+          left={(props) => <Avatar.Icon {...props} icon="face-man" />}
+          right={(props) => (
+            <IconButton {...props} icon="plus" onPress={() => {}} />
+          )}
+        />
+      </Card>
+      <Divider style={{ marginBlock: 12 }}></Divider>
+      <Card mode="contained" onPress={() => navigate("Measurements")}>
+        <Card.Title
+          title="Add measurements"
+          subtitle="Card Subtitle"
+          left={(props) => <Avatar.Icon {...props} icon="ruler" />}
+          right={(props) => (
+            <IconButton {...props} icon="plus" onPress={() => {}} />
+          )}
+        />
+      </Card>
     </ScrollView>
   );
 };
 
 const Styles = StyleSheet.create({
-  container: { display: 'flex', flex: 1 },
   scrollView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red',
+    padding: 12,
   },
 });
 
