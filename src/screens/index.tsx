ad6b2e6a5cefb547b896791581af2e9cf38e7089
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2025-07-26 18:07:55 +0500

    Updated stack navigator screen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -7,29 +7,32 @@ import SignIn from './authentication/signIn';
 import ForgotPassword from './authentication/forgotPassword';
 
 import Dashboard from './secure/dashboard';
-import Measurements from './secure/measurements';
-import { StatusBar } from 'react-native';
-import { Button } from 'react-native-paper';
-import ScreenWrapper from '../components/ScreenWrapper';
+import Measurements from "./secure/measurements";
+import { Button } from "react-native-paper";
+import ScreenWrapper from "../components/ScreenWrapper";
+import ShopType from "./secure/shop-type";
+import AddCustomer from "./secure/add-customer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default () => {
   const uid = undefined;
   const Stack = createNativeStackNavigator();
 
-  return (
-    <>
-      <ScreenWrapper>
-        <Button icon="camera" contentStyle={{ backgroundColor: 'red' }}>
-          Press me
-        </Button>
-      </ScreenWrapper>
+  console.log(AsyncStorage.getItem("shop_type"));
 
-      {/* <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-        </Stack.Navigator>
-      </NavigationContainer> */}
-    </>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="ShopType"
+          component={ShopType}
+          options={{ title: "Shop Type" }}
+        />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="Measurements" component={Measurements} />
+        <Stack.Screen name="AddCustomer" component={AddCustomer} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 
   // return (
