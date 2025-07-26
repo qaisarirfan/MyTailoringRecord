6bfb409c62ac0377fe236e093f295603106eb6f7
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2025-07-26 18:07:55 +0500

    Updated main app screen

--- App.tsx
+++ App.tsx
@@ -1,34 +1,21 @@
-import React, { StrictMode } from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import persistStore from 'redux-persist/lib/persistStore';
-
-import Screens from './src/screens';
-import configureStore from './src/redux/configureStore';
-import { PaperProvider } from 'react-native-paper';
-import {
-  SafeAreaInsetsContext,
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
-import { NativeModules, Platform, StatusBar } from 'react-native';
-
-const store = configureStore();
-const persistor = persistStore(store);
+import React, { StrictMode, useEffect } from "react";
+import { PaperProvider } from "react-native-paper";
+import { Platform, StatusBar } from "react-native";
+import Screens from "./src/screens";
 
 const App: React.FC = () => {
-  const { StatusBarManager } = NativeModules;
-  const statusBarHeight = StatusBarManager.HEIGHT;
-
-  console.log(statusBarHeight);
-
-  // const headerHeight = useHeaderHeight();
   return (
     <StrictMode>
-      <Provider store={store}>
-        <PaperProvider settings={{ rippleEffectEnabled: true }}>
-          <Screens />
-        </PaperProvider>
-      </Provider>
+      <PaperProvider settings={{ rippleEffectEnabled: true }}>
+        <StatusBar
+          barStyle={Platform.OS === "ios" ? "dark-content" : "light-content"}
+          backgroundColor={Platform.OS === "android" ? "#000000" : undefined}
+          translucent={false}
+          animated
+          hidden={false}
+        />
+        <Screens />
+      </PaperProvider>
     </StrictMode>
   );
 };
